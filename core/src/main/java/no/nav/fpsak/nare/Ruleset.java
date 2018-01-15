060983cf17532480beb66cdd7a59112381f34ebb
Bjørn Thore Storesund <bjorn.thore.storesund@nav.no>
2018-01-15 11:09:54 +0100

    NOJIRA Refaktorering

--- core/src/main/java/no/nav/fpsak/nare/Ruleset.java
+++ core/src/main/java/no/nav/fpsak/nare/Ruleset.java
@@ -24,14 +24,55 @@ public class Ruleset<V> {
         return ConditionalOrSpecification.<V> regel(id, beskrivelse);
     }
 
+    /**
+     * Realiserer flyten if <betingelse> then <then-spesifikasjon> else <else-spesifikasjon> for beregningsregel
+     * @param testSpec
+     * @param thenSpec
+     * @param elseSpec
+     * @return
+     */
     public Specification<V> beregningHvisRegel(Specification<V> testSpec, Specification<V> thenSpec, Specification<V> elseSpec) {
         return new ComputationalIfSpecification<>(testSpec, thenSpec, elseSpec);
     }
 
+    /**
+     * Realiserer sekvens av to spesifikasjoner der bare den siste har betydning for videre flyt
+     * @param id
+     * @param beskrivelse
+     * @param spec1
+     * @param spec2
+     * @return
+     */
     public Specification<V> beregningsRegel(String id, String beskrivelse, Specification<V> spec1, Specification<V> spec2) {
         return new SequenceSpecification<>(id, beskrivelse, spec1, spec2);
     }
 
+    /**
+     * Realiserer sekvens av N spesifikasjoner der bare den siste har betydning for videre flyt
+     * @param id
+     * @param beskrivelse
+     * @param spec1list
+     * @param spec2
+     * @return
+     */
+    public Specification<V> beregningsRegel(String id, String beskrivelse, List<Specification<V>> spec1list, Specification<V> spec2) {
+        List<Specification<V>> specs = new ArrayList<>();
+        specs.addAll(spec1list);
+        specs.add(spec2);
+        return new SequenceSpecification<>(id, beskrivelse, specs);
+    }
+
+    /**
+     * Realiserer sekvens der en regel utføres N ganger, etterfulgt av en spesifikasjon som har betydning for videre flyt
+     * @param id
+     * @param beskrivelse
+     * @param spec1 regel som utføres 1 gang for hvert element i argumentlisten, med argumentbeskrivelse + argumentverdi som serviceargument
+     * @param regelmodell
+     * @param argumentBeskrivelse
+     * @param args1 argumentlisten, inneholder N argumenter
+     * @param spec2 utføres 1 gang etter at spec1 er utført N ganger
+     * @return
+     */
     public Specification<V> beregningsRegel(String id, String beskrivelse, Class<? extends DynamicRuleService<V>> spec1, V regelmodell, String argumentBeskrivelse, List<? extends Object> args1, Specification<V> spec2) {
         Objects.requireNonNull(spec1, "spec1");
         Objects.requireNonNull(args1, "args1");
@@ -43,38 +84,20 @@ public class Ruleset<V> {
         args1.forEach(arg -> {
             DynamicRuleService<V> ny;
             try {
-                ny = spec1.newInstance();  // TODO (bts) bruk reflection med regelmodell som argument, fjerne constructor uten argument fra regelklassene
-                ny.setRegelmodell(regelmodell);
+                ny = opprettSpesifikasjon(spec1, regelmodell);
                 ny.medServiceArgument(new ServiceArgument(argumentBeskrivelse, arg));
                 specs.add(ny.getSpecification());
             } catch (InstantiationException | IllegalAccessException e) {
-                // TODO Auto-generated catch block
-                e.printStackTrace();
-                throw new IllegalStateException("NARE feil");
+                throw new IllegalStateException("Utviklerfeil: DynamicRuleService mangler public default constructor");
             }
         });
         specs.add(spec2);
         return new SequenceSpecification<V>(id, beskrivelse, specs);
     }
-        
-//        Specification<V> denne = null;
-//        try {
-//            for (Object arg : args1) {
-//                DynamicRuleService<V> ny;
-//                ny = spec1.newInstance();  // TODO (bts) bruk reflection med regelmodell som argument, fjerne constructor uten argument fra regelklassene
-//                ny.setRegelmodell(regelmodell);
-//                ny.medArgument(arg);
-//                if (denne == null) {
-//                    denne = ny.getSpecification();
-//                } else {
-//                    Specification<V> neste = ny.getSpecification();
-//                    denne = new SequenceSpecification<V>(id, beskrivelse, denne, neste);
-//                }
-//            }
-//            return new SequenceSpecification<>(id, beskrivelse, denne, spec2);
-//        } catch (InstantiationException | IllegalAccessException e) {
-//            // TODO Auto-generated catch block
-//            e.printStackTrace();
-//            return null;
-//        }
+
+    private DynamicRuleService<V> opprettSpesifikasjon(Class<? extends DynamicRuleService<V>> spec1, V regelmodell) throws InstantiationException, IllegalAccessException {
+        DynamicRuleService<V> ny = spec1.newInstance();
+        ny.setRegelmodell(regelmodell);
+        return ny;
+    }
 }
