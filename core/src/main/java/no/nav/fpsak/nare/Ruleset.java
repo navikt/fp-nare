291b5276261a68cec2308402ea21d73352f7fc64
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-01-16 11:45:20 +0100

    improve pom and build (#18)

--- core/src/main/java/no/nav/fpsak/nare/Ruleset.java
+++ core/src/main/java/no/nav/fpsak/nare/Ruleset.java
@@ -1,5 +1,6 @@
 package no.nav.fpsak.nare;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -21,41 +22,50 @@ public class Ruleset<V> {
     }
 
     public Builder<V> hvisRegel(String id, String beskrivelse) {
-        return ConditionalOrSpecification.<V> regel(id, beskrivelse);
+        return ConditionalOrSpecification.<V>regel(id, beskrivelse);
     }
 
     /**
-     * Realiserer flyten if <betingelse> then <then-spesifikasjon> else <else-spesifikasjon> for beregningsregel
+     * Realiserer flyten if <betingelse> then <then-spesifikasjon> else
+     * <else-spesifikasjon> for beregningsregel
+     * 
      * @param testSpec
      * @param thenSpec
      * @param elseSpec
      * @return
      */
-    public Specification<V> beregningHvisRegel(Specification<V> testSpec, Specification<V> thenSpec, Specification<V> elseSpec) {
+    public Specification<V> beregningHvisRegel(Specification<V> testSpec, Specification<V> thenSpec,
+            Specification<V> elseSpec) {
         return new ComputationalIfSpecification<>(testSpec, thenSpec, elseSpec);
     }
 
     /**
-     * Realiserer sekvens av to spesifikasjoner der bare den siste har betydning for videre flyt
+     * Realiserer sekvens av to spesifikasjoner der bare den siste har betydning for
+     * videre flyt
+     * 
      * @param id
      * @param beskrivelse
      * @param spec1
      * @param spec2
      * @return
      */
-    public Specification<V> beregningsRegel(String id, String beskrivelse, Specification<V> spec1, Specification<V> spec2) {
+    public Specification<V> beregningsRegel(String id, String beskrivelse, Specification<V> spec1,
+            Specification<V> spec2) {
         return new SequenceSpecification<>(id, beskrivelse, spec1, spec2);
     }
 
     /**
-     * Realiserer sekvens av N spesifikasjoner der bare den siste har betydning for videre flyt
+     * Realiserer sekvens av N spesifikasjoner der bare den siste har betydning for
+     * videre flyt
+     * 
      * @param id
      * @param beskrivelse
      * @param spec1list
      * @param spec2
      * @return
      */
-    public Specification<V> beregningsRegel(String id, String beskrivelse, List<Specification<V>> spec1list, Specification<V> spec2) {
+    public Specification<V> beregningsRegel(String id, String beskrivelse, List<Specification<V>> spec1list,
+            Specification<V> spec2) {
         List<Specification<V>> specs = new ArrayList<>();
         specs.addAll(spec1list);
         specs.add(spec2);
@@ -63,17 +73,22 @@ public class Ruleset<V> {
     }
 
     /**
-     * Realiserer sekvens der en regel utføres N ganger, etterfulgt av en spesifikasjon som har betydning for videre flyt
+     * Realiserer sekvens der en regel utføres N ganger, etterfulgt av en
+     * spesifikasjon som har betydning for videre flyt
+     * 
      * @param id
      * @param beskrivelse
-     * @param spec1 regel som utføres 1 gang for hvert element i argumentlisten, med argumentbeskrivelse + argumentverdi som serviceargument
+     * @param spec1               regel som utføres 1 gang for hvert element i
+     *                            argumentlisten, med argumentbeskrivelse +
+     *                            argumentverdi som serviceargument
      * @param regelmodell
      * @param argumentBeskrivelse
-     * @param args1 argumentlisten, inneholder N argumenter
-     * @param spec2 utføres 1 gang etter at spec1 er utført N ganger
+     * @param args1               argumentlisten, inneholder N argumenter
+     * @param spec2               utføres 1 gang etter at spec1 er utført N ganger
      * @return
      */
-    public Specification<V> beregningsRegel(String id, String beskrivelse, Class<? extends DynamicRuleService<V>> spec1, V regelmodell, String argumentBeskrivelse, List<? extends Object> args1, Specification<V> spec2) {
+    public Specification<V> beregningsRegel(String id, String beskrivelse, Class<? extends DynamicRuleService<V>> spec1,
+            V regelmodell, String argumentBeskrivelse, List<? extends Object> args1, Specification<V> spec2) {
         Objects.requireNonNull(spec1, "spec1");
         Objects.requireNonNull(args1, "args1");
         Objects.requireNonNull(spec2, "spec2");
@@ -88,16 +103,24 @@ public class Ruleset<V> {
                 ny.medServiceArgument(new ServiceArgument(argumentBeskrivelse, arg));
                 specs.add(ny.getSpecification());
             } catch (InstantiationException | IllegalAccessException e) {
-                throw new IllegalStateException("Utviklerfeil: DynamicRuleService mangler public default constructor");
+                throw new IllegalStateException("Utviklerfeil: DynamicRuleService mangler public default constructor",
+                        e);
             }
         });
         specs.add(spec2);
         return new SequenceSpecification<V>(id, beskrivelse, specs);
     }
 
-    private DynamicRuleService<V> opprettSpesifikasjon(Class<? extends DynamicRuleService<V>> spec1, V regelmodell) throws InstantiationException, IllegalAccessException {
-        DynamicRuleService<V> ny = spec1.newInstance();
-        ny.setRegelmodell(regelmodell);
-        return ny;
+    private DynamicRuleService<V> opprettSpesifikasjon(Class<? extends DynamicRuleService<V>> spec1, V regelmodell)
+            throws InstantiationException, IllegalAccessException {
+
+        try {
+            DynamicRuleService<V> ny = spec1.getDeclaredConstructor().newInstance();
+            ny.setRegelmodell(regelmodell);
+            return ny;
+        } catch (IllegalArgumentException | InvocationTargetException | NoSuchMethodException | SecurityException e) {
+            throw new IllegalStateException("Utviklerfeil: DynamicRuleService mangler public default constructor", e);
+        }
+
     }
 }
