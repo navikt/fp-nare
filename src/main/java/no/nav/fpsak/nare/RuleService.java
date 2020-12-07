cf0103652b8ee87eea91b4c100350d03388efec2
Jan-Olav Eide <Jan-Olav.Eide@nav.no>
2020-12-07 12:08:17 +0100

    Update RuleService.java

--- src/main/java/no/nav/fpsak/nare/RuleService.java
+++ src/main/java/no/nav/fpsak/nare/RuleService.java
@@ -16,8 +16,7 @@ import no.nav.fpsak.nare.specification.Specification;
  * @param <T> type av input
  */
 public interface RuleService<T> {
-    
-    
+   
 
     /**
      * Evaluer regel, all informasjon om kjøring returneres i Evaluation objektet.
