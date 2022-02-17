9814039d4a9db25a2e325cbfbcf02121df922f47
Marius Glittum <marius.glittum@nav.no>
2022-02-17 19:24:16 +0100

    Fjerner doclet og tilhørende avhengighet

--- src/main/java/no/nav/fpsak/nare/doc/doclet/MarkupOutput.java
+++
@@ -1,8 +0,0 @@
-package no.nav.fpsak.nare.doc.doclet;
-
-import io.github.swagger2markup.markup.builder.MarkupDocBuilder;
-
-public interface MarkupOutput {
-
-    void apply(int sectionLevel, MarkupDocBuilder doc);
-}
