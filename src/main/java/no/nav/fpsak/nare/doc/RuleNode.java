3bb40d164b5e35e377f68a620115cca20d570408
Tore Langedal Endestad <tore.langedal.endestad@nav.no>
2023-01-25 11:09:49 +0100

    Stabil serialisering og mulighet for å oppgi versjoner

--- src/main/java/no/nav/fpsak/nare/doc/RuleNode.java
+++ src/main/java/no/nav/fpsak/nare/doc/RuleNode.java
@@ -6,22 +6,39 @@ import no.nav.fpsak.nare.evaluation.Operator;
 
 @SuppressWarnings("unused")
 public class RuleNode {
-    final String id = UUID.randomUUID().toString();
+    final String id;
 
     private final String ruleId;
     private final String ruleDescription;
     private final Operator operator;
-
     private RuleDescription rule;
 
+    @Deprecated(forRemoval = true, since = "2.5.0")
     public RuleNode(String ruleId, String ruleDescription, Operator operator) {
-        super();
+        this.id = UUID.randomUUID().toString();
         this.ruleId = ruleId;
         this.ruleDescription = ruleDescription;
         this.operator = operator;
     }
 
+    @Deprecated(forRemoval = true, since = "2.5.0")
     public RuleNode(RuleDescription node) {
+        this.id = UUID.randomUUID().toString();
+        this.ruleId = node.getRuleIdentification();
+        this.ruleDescription = node.getRuleDescription();
+        this.operator = node.getOperator();
+        this.rule = node;
+    }
+
+    public RuleNode(String id, String ruleId, String ruleDescription, Operator operator) {
+        this.id = id;
+        this.ruleId = ruleId;
+        this.ruleDescription = ruleDescription;
+        this.operator = operator;
+    }
+
+    public RuleNode(String id, RuleDescription node) {
+        this.id = id;
         this.ruleId = node.getRuleIdentification();
         this.ruleDescription = node.getRuleDescription();
         this.operator = node.getOperator();
