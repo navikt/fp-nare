0161532da89ae73b209d517efbb721b3e611efd1
Jens-Otto Larsen <jens-otto.larsen@nav.no>
2025-11-02 19:24:23 +0100

    chore: bruk felles parent pom for deps og bygg

--- src/test/java/no/nav/fpsak/nare/specification/modrekvote/SinglespecificationTest.java
+++ src/test/java/no/nav/fpsak/nare/specification/modrekvote/SinglespecificationTest.java
@@ -1,14 +1,5 @@
 package no.nav.fpsak.nare.specification.modrekvote;
 
-import static no.nav.fpsak.nare.specification.modrekvote.input.Rolle.MOR;
-import static no.nav.fpsak.nare.specification.modrekvote.regler.HarRettTilForeldrePenger.harRettTilForeldrePenger;
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.util.stream.Collectors;
-
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 import no.nav.fpsak.nare.RuleService;
 import no.nav.fpsak.nare.Ruleset;
 import no.nav.fpsak.nare.evaluation.Evaluation;
@@ -20,14 +11,20 @@ import no.nav.fpsak.nare.specification.modrekvote.input.Person;
 import no.nav.fpsak.nare.specification.modrekvote.input.Rolle;
 import no.nav.fpsak.nare.specification.modrekvote.input.Soknad;
 import no.nav.fpsak.nare.specification.modrekvote.input.Uttaksplan;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-public class SinglespecificationTest {
+import static no.nav.fpsak.nare.specification.modrekvote.input.Rolle.MOR;
+import static no.nav.fpsak.nare.specification.modrekvote.regler.HarRettTilForeldrePenger.harRettTilForeldrePenger;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SinglespecificationTest {
 
     Person far = new Person("Truls", Rolle.FAR, "Nerd", 500000, 800, "Klofta", true);
     Person mor = new Person("Guro", Rolle.MOR, "Prosjektleder", 600000, 24, "Oslo", true);
 
     @Test
-    public void skal_evaluere_regel_med_kun_ett_leaf_ved_roten() throws Exception {
+    void skal_evaluere_regel_med_kun_ett_leaf_ved_roten() {
         mor.setUttaksplan(Uttaksplan.SENERE);
         Soknad soknad = Soknad.fodselSøknad(mor).medSøker(far);
 
@@ -43,7 +40,7 @@ public class SinglespecificationTest {
         EvaluationSummary evaluationSummary = new EvaluationSummary(evaluation);
         var leafReasons = evaluationSummary.leafEvaluations().stream()
                 .map(Evaluation::getOutcome)
-                .collect(Collectors.toList());
+                .toList();
         Assertions.assertThat(leafReasons).hasSize(1);
         Assertions.assertThat(evaluationSummary.singleLeafEvaluation(Resultat.JA)).isPresent();
 
